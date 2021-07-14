95d6e0a74fb39166b38334e890d5942d7633082e
Peter Treitler <peter.treitler@gmail.com>
2021-07-15 00:42:24 +0200

    solve problem 14

---
+++ src/14.kt
@@ -0,0 +1,43 @@
+/**
+ * The following iterative sequence is defined for the set of positive integers:
+ *
+ * n → n/2 (n is even)
+ * n → 3n + 1 (n is odd)
+ *
+ * Using the rule above and starting with 13, we generate the following sequence:
+ *
+ * 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+ * It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms.
+ * Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+ *
+ * Which starting number, under one million, produces the longest chain?
+ *
+ * NOTE: Once the chain starts the terms are allowed to go above one million.
+ */
+
+fun main() {
+    val limit = 1_000_000L
+    var maxSequenceLength = 0
+    var maxSequenceNumber = 0L
+    (1 until limit).forEach {
+        val length = getCollatzSequenceLength(it)
+        if (length > maxSequenceLength) {
+            maxSequenceLength = length
+            maxSequenceNumber = it
+        }
+    }
+
+    println("$maxSequenceNumber: Length of $maxSequenceLength")
+}
+
+fun getCollatzSequenceLength(number: Long): Int {
+    var current = number
+    var length = 1
+    while (current != 1L) {
+        current = current.nextCollatzNumber()
+        length++
+    }
+    return length
+}
+
+fun Long.nextCollatzNumber() = if (this % 2 == 0L) this / 2 else 3 * this + 1
